7f0ccd34b6dd40eef333615c9d9d1cdf3f1ace15
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2023-08-22 15:59:05 +0900

    AdministratorService.javaにメソッドを追加

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -11,4 +12,14 @@ import com.example.repository.AdministratorRepository;
 public class AdministratorService {
   @Autowired
   private AdministratorRepository administratorRepository;
+
+  /**
+   * 管理者情報を挿入する(administratorRepositoryのinsert()メソッドを呼ぶ)
+   * 
+   * @param administrator
+   * @return なし
+   */
+  public void insert(Administrator administrator) {
+    administratorRepository.insert(administrator);
+  }
 }
