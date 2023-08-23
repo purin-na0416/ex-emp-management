437156da7da3de9468fdee7b330c9d6789f81642
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2023-08-23 10:46:47 +0900

    EmployeeService.java作成

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+
+@Service
+@Transactional
+public class EmployeeService {
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  /**
+   * 従業員情報を全件取得する(employeeRepositoryのfindAll()を呼び出す)
+   * 
+   * @return
+   */
+  public List<Employee> showList() {
+    List<Employee> employeeList = employeeRepository.findAll();
+    return employeeList;
+  }
+}
