34b0cd2a3b724cdad1d188146f981c2ae5c8cf5d
Natsuki Fujii <natsuki.fujii@rakus-partners.co.jp>
2023-08-23 10:16:00 +0900

    AdministratorController.javaにメソッドを追加

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,16 @@ import com.example.form.InsertAdministratorForm;
 import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
   @Autowired
   private AdministratorService administratorService;
+
+  @Autowired
+  private HttpSession session;
 
   @GetMapping("/toInsert")
   /**
@@ -57,5 +63,19 @@ public class AdministratorController {
    */
   public String toLogin(LoginForm form) {
     return "administrator/login";
+  }
+
+  @PostMapping("/login")
+  public String login(LoginForm form, Model model) {
+    Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+
+    if(administrator == null) {
+      String message = "メールアドレスまたはパスワードが不正です。";
+      model.addAttribute("error", message);
+      return "administrator/login";
+    } else {
+      session.setAttribute("administratorName", administrator);
+      return "redirect:/employee/showList";
+    }
   }
 }
